0897c7b6b9b6d33b1a16e880e36e6787025092b9
adithi2905 <101107241+adithi2905@users.noreply.github.com>
2025-05-16 00:27:28 -0400

    Added a java stream based logic

--- src/main/java/com/ecober/domain/service/DriverMatchingService.java
+++ src/main/java/com/ecober/domain/service/DriverMatchingService.java
@@ -6,17 +6,17 @@ import org.springframework.stereotype.Service;
 import com.ecober.adapter.Dto.DriverDTO;
 
 @Service
-public class FetchNearestRiderService
+public class DriverMatchingService
 {
 
     private final List<DriverDTO>availableDrivers=List.of(new DriverDTO("Alice", "KA01X123", "D001", true, "Indiranagar", "EV", 18.5, 4.5, 12.0),
         new DriverDTO("Bob", "KA02Y456", "D002", true, "MG Road", "SUV", 10.0, 4.2, 20.0),
         new DriverDTO("Charlie", "KA03Z789", "D003", false, "Indiranagar", "Sedan", 15.0, 3.9, 5.0));
 
-        public DriverDTO fetchNearestRider(String riderPickupLocation, String riderDropOffLocation, String preferredVehicleType, boolean wilingToPool) {
+        public DriverDTO fetchNearestDriver(String riderPickupLocation, String riderDropOffLocation, String preferredVehicleType, boolean wilingToPool) {
             for(DriverDTO driver:availableDrivers)
             {
-                if(driver.getDriverLocation().equalsIgnoreCase(riderDropOffLocation) &&
+                if(driver.getDriverLocation().equalsIgnoreCase(riderPickupLocation) &&
                 (driver.getVehicleType().equalsIgnoreCase(preferredVehicleType)) &&
                 (driver.isVerifiedDriver()))
                 return driver;
