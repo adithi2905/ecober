eb8a3213ef961ec8436c30c849d1755f8417ee25
adithi2905 <101107241+adithi2905@users.noreply.github.com>
2025-05-28 23:10:13 -0400

    Added service to find the coordinates from location

--- backend/src/main/java/com/ecober/domain/service/RiderRequestService.java
+++ backend/src/main/java/com/ecober/domain/service/RiderRequestService.java
@@ -1,16 +1,12 @@
 package com.ecober.domain.service;
 
-import com.ecober.adapter.Dto.DistanceDurationDTO;
 import com.ecober.adapter.Dto.DriverDTO;
 import com.ecober.adapter.Dto.RiderDTO;
 import com.ecober.adapter.mapper.DriverMapper;
 import com.ecober.domain.model.Driver;
 import com.ecober.domain.model.Route;
-import com.ecober.domain.model.Trip;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
 
 @Service
 public class RiderRequestService {
@@ -27,28 +23,44 @@ public class RiderRequestService {
     @Autowired
     private DriverMapper driverMapper;
 
+    @Autowired
+    private GeocodingService geocodingService;
+
     public DriverDTO handleRideRequest(RiderDTO riderDTO) {
 
-        // Step 1: Compute Route & Emission
+        double[] pickupLatLong = geocodingService.getLatAndLong(riderDTO.getRiderPickupLocation());
+        double[] dropoffLatLong = geocodingService.getLatAndLong(riderDTO.getRiderDropOffLocation());
+
+        RiderDTO updatedRiderDTO = RiderDTO.builder()
+                .riderId(riderDTO.getRiderId())
+                .riderName(riderDTO.getRiderName())
+                .riderPickupLocation(riderDTO.getRiderPickupLocation())
+                .riderDropOffLocation(riderDTO.getRiderDropOffLocation())
+                .pickupLatitude(pickupLatLong[0])
+                .pickupLongitude(pickupLatLong[1])
+                .dropoffLatitude(dropoffLatLong[0])
+                .dropoffLongitude(dropoffLatLong[1])
+                .preferredVehicleType(riderDTO.getPreferredVehicleType())
+                .willingToPool(riderDTO.isWillingToPool())
+                .build();
+
         Route route = routeService.getOrCreateRoute(
-                riderDTO.getRiderPickupLocation(),
-                riderDTO.getRiderDropOffLocation(),
-                riderDTO.getPickupLatitude(),
-                riderDTO.getPickupLongitude(),
-                riderDTO.getDropoffLatitude(),
-                riderDTO.getDropoffLongitude(),
-                riderDTO.getPreferredVehicleType()
+                updatedRiderDTO.getRiderPickupLocation(),
+                updatedRiderDTO.getRiderDropOffLocation(),
+                updatedRiderDTO.getPickupLatitude(),
+                updatedRiderDTO.getPickupLongitude(),
+                updatedRiderDTO.getDropoffLatitude(),
+                updatedRiderDTO.getDropoffLongitude(),
+                updatedRiderDTO.getPreferredVehicleType()
         );
 
-        // Step 2: Find Best Driver
         Driver driver = driverMatchingService.findBestDriver(
-                riderDTO.getRiderPickupLocation(),
-                riderDTO.getPreferredVehicleType(),
-                riderDTO.isWillingToPool()
+                updatedRiderDTO.getRiderPickupLocation(),
+                updatedRiderDTO.getPreferredVehicleType(),
+                updatedRiderDTO.isWillingToPool()
         );
 
-        // Step 3: Create Trip
-        tripService.createTrip(riderDTO.getRiderId(), driver.getDriverId().toString(), route);
+        tripService.createTrip(updatedRiderDTO.getRiderId(), driver.getDriverId().toString(), route);
 
         return driverMapper.toDto(driver);
     }
